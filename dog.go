08b6b0b2813cd871c15b21e2ae36af3ca2d8a1e8
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-05-05 17:11:54 -0700

    dog fix

--- dog.go
+++ dog.go
@@ -1,7 +1,9 @@
+package dog
+
 import (
 	"strings"
 )
 
 func WhenGrownUp(s string) {
-	return "When the puppy grows up it says: " + string.ToUpper(s)
+	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
